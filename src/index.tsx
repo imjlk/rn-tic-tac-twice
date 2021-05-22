2fe2fa88559851822a4fc086310b8d4c47b4fc19
imjlk <i@m.junglei.kim>
2021-05-22 19:25:21 +0900

    Aliasing paths - babel-plugin-module-resolver, tsconfig 수정

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { Game, Home } from "./screens";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { Game, Home } from "@screens";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={require("@assets/favicon.png")} />
+      <Home />
+      <Game />
     </View>
   );
 }
