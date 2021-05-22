334f741650eaa6b9f9b11ac6e8e885628863a511
imjlk <i@m.junglei.kim>
2021-05-22 16:37:36 +0900

    husky@^4 설치

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,8 +8,6 @@ module.exports = {
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
     "prettier",
-    "prettier/@typescript-eslint",
-    "prettier/react",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
