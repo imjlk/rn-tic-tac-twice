7167f9caddbe1a6d11fe5410b1b0e1a3ae33167a
imjlk <i@m.junglei.kim>
2021-05-22 19:52:05 +0900

    adjust expo-google-fonts

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,37 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
 import { Game, Home } from "@screens";
+import {
+  useFonts,
+  BlackHanSans_400Regular,
+} from "@expo-google-fonts/black-han-sans";
+import {
+  useFonts as useNanumFonts,
+  NanumGothic_400Regular,
+  NanumGothic_700Bold,
+  NanumGothic_800ExtraBold,
+} from "@expo-google-fonts/nanum-gothic";
 
 export default function App() {
+  const [fontLoaded] = useFonts({
+    BlackHanSans_400Regular,
+  });
+  const [nanumFontsLoaded] = useNanumFonts({
+    NanumGothic_400Regular,
+    NanumGothic_700Bold,
+    NanumGothic_800ExtraBold,
+  });
+  if (!nanumFontsLoaded || !fontLoaded) return null;
   return (
     <View style={styles.container}>
-      <Image source={require("@assets/favicon.png")} />
-      <Home />
-      <Game />
+      <Text
+        style={{
+          fontSize: 25,
+          fontFamily: "NanumGothic_400Regular",
+        }}
+      >
+        Hello World
+      </Text>
     </View>
   );
 }
