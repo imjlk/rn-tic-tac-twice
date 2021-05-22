377b81ab2bef05e2e8bd22c8c35cd5ab5c4239d8
imjlk <i@m.junglei.kim>
2021-05-22 21:22:19 +0900

    add react navigation

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,12 @@
 import React, { ReactElement } from "react";
-import { StyleSheet, View } from "react-native";
-import { Text, AppBootstrap } from "@components";
+import { StyleSheet } from "react-native";
+import { AppBootstrap } from "@components";
+import Navigator from "@config/navigator";
 
 export default function App(): ReactElement {
   return (
     <AppBootstrap>
-      <View style={styles.container}>
-        <Text
-          style={{
-            fontSize: 25,
-          }}
-          onPress={() => {
-            alert(true);
-          }}
-        >
-          Nesting is <Text weight={800}>working </Text>well
-        </Text>
-      </View>
+      <Navigator />
     </AppBootstrap>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- src/screens/game/index.tsx
+++ src/screens/game/index.tsx
@@ -1,11 +1,23 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { ReactElement } from "react";
+import { View, Text, Button } from "react-native";
 import styles from "./game.styles";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { StackNavigatorParams } from "@config/navigator";
 
-export default function Game() {
+type GameProps = {
+  navigation: StackNavigationProp<StackNavigatorParams, "Game">;
+};
+
+export default function Game({ navigation }: GameProps): ReactElement {
   return (
     <View style={styles.container}>
       <Text>Game</Text>
+      <Button
+        title="Go back"
+        onPress={() => {
+          navigation.goBack();
+        }}
+      />
     </View>
   );
 }
