81cf9870e62dc777dcbf530476117ae8e090c4a4
imjlk <i@m.junglei.kim>
2021-05-28 14:36:37 +0900

    add sound, haptic features

--- src/screens/game/index.tsx
+++ src/screens/game/index.tsx
@@ -1,10 +1,12 @@
-import React, { ReactElement, useEffect, useState } from "react";
+import React, { ReactElement, useEffect, useState, useRef } from "react";
 import { SafeAreaView } from "react-native";
 import styles from "./game.styles";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { StackNavigatorParams } from "@config/navigator";
 import { BackgroundGradient, Board } from "@components";
 import { BoardState, getBestMove, isEmpty, isTerminal } from "@utils";
+import { Audio } from "expo-av";
+import * as Haptics from "expo-haptics";
 
 type GameProps = {
   navigation: StackNavigationProp<StackNavigatorParams, "Game">;
@@ -21,8 +23,9 @@ export default function Game({ navigation }: GameProps): ReactElement {
     Math.random() < 0.5 ? "HUMAN" : "BOT"
   );
   const [isHumanMaximizing, setIsHumanMaximizing] = useState<boolean>(true);
+  const popSoundRef = useRef<Audio.Sound | null>(null);
+  const pop2SoundRef = useRef<Audio.Sound | null>(null);
 
-  console.log(turn, isHumanMaximizing);
   const gameResult = isTerminal(state);
 
   const insertCell = (cell: number, symbol: "x" | "o"): void => {
@@ -31,6 +34,16 @@ export default function Game({ navigation }: GameProps): ReactElement {
 
     stateCopy[cell] = symbol;
     setState(stateCopy);
+
+    try {
+      symbol === "x"
+        ? popSoundRef.current?.replayAsync()
+        : pop2SoundRef.current?.replayAsync();
+      Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+    } catch (error) {
+      // TODO: Adjust sentry
+      console.log(error);
+    }
   };
   const handleOnCellPressed = (cell: number): void => {
     if (turn !== "HUMAN") return;
@@ -61,6 +74,27 @@ export default function Game({ navigation }: GameProps): ReactElement {
       }
     }
   }, [state, turn]);
+
+  useEffect(() => {
+    // load sounds
+    const popSoundObject = new Audio.Sound();
+    const pop2SoundObject = new Audio.Sound();
+
+    const loadSounds = async () => {
+      /* eslint-disable @typescript-eslint/no-var-requires */
+      await popSoundObject.loadAsync(require("@assets/pop-1.wav"));
+      popSoundRef.current = popSoundObject;
+      await pop2SoundObject.loadAsync(require("@assets/pop-2.wav"));
+      pop2SoundRef.current = pop2SoundObject;
+    };
+
+    loadSounds();
+    return () => {
+      // unload sounds
+      popSoundObject && popSoundObject.unloadAsync();
+      pop2SoundObject && pop2SoundObject.unloadAsync();
+    };
+  }, []);
 
   return (
     <BackgroundGradient>
