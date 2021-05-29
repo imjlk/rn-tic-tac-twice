b1d0f7ed4dac65d943eab76c993dd16878844e85
imjlk <i@m.junglei.kim>
2021-05-29 18:40:05 +0900

    add useSounds custom hook

--- src/screens/game/index.tsx
+++ src/screens/game/index.tsx
@@ -1,12 +1,17 @@
-import React, { ReactElement, useEffect, useState, useRef } from "react";
+import React, { ReactElement, useEffect, useState } from "react";
 import { SafeAreaView } from "react-native";
 import styles from "./game.styles";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { StackNavigatorParams } from "@config/navigator";
 import { BackgroundGradient, Board } from "@components";
-import { BoardState, Cell, getBestMove, isEmpty, isTerminal } from "@utils";
-import { Audio } from "expo-av";
-import * as Haptics from "expo-haptics";
+import {
+  BoardState,
+  Cell,
+  getBestMove,
+  isEmpty,
+  isTerminal,
+  useSounds,
+} from "@utils";
 
 type GameProps = {
   navigation: StackNavigationProp<StackNavigatorParams, "Game">;
@@ -23,11 +28,7 @@ export default function Game({ navigation }: GameProps): ReactElement {
     Math.random() < 0.5 ? "HUMAN" : "BOT"
   );
   const [isHumanMaximizing, setIsHumanMaximizing] = useState<boolean>(true);
-  const popSoundRef = useRef<Audio.Sound | null>(null);
-  const pop2SoundRef = useRef<Audio.Sound | null>(null);
-  const winSoundRef = useRef<Audio.Sound | null>(null);
-  const lossSoundRef = useRef<Audio.Sound | null>(null);
-  const drawSoundRef = useRef<Audio.Sound | null>(null);
+  const playSound = useSounds();
 
   const gameResult = isTerminal(state);
 
@@ -39,12 +40,8 @@ export default function Game({ navigation }: GameProps): ReactElement {
     setState(stateCopy);
 
     try {
-      symbol === "x"
-        ? popSoundRef.current?.replayAsync()
-        : pop2SoundRef.current?.replayAsync();
-      Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+      symbol === "x" ? playSound("pop1") : playSound("pop2");
     } catch (error) {
-      // TODO: Adjust sentry
       console.log(error);
     }
   };
@@ -71,30 +68,15 @@ export default function Game({ navigation }: GameProps): ReactElement {
 
       const winner = getWinner(gameResult.winner);
       if (winner === "HUMAN") {
-        try {
-          winSoundRef.current?.replayAsync();
-          Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success);
-        } catch (error) {
-          console.log(error);
-        }
+        playSound("win");
         alert("You Won");
       }
       if (winner === "BOT") {
-        try {
-          lossSoundRef.current?.replayAsync();
-          Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error);
-        } catch (error) {
-          console.log(error);
-        }
+        playSound("loss");
         alert("You Lost");
       }
       if (winner === "DRAW") {
-        try {
-          drawSoundRef.current?.replayAsync();
-          Haptics.notificationAsync(Haptics.NotificationFeedbackType.Warning);
-        } catch (error) {
-          console.log(error);
-        }
+        playSound("draw");
         alert("It's a Draw");
       }
     } else {
@@ -116,39 +98,6 @@ export default function Game({ navigation }: GameProps): ReactElement {
       }
     }
   }, [state, turn]);
-
-  useEffect(() => {
-    // load sounds
-    const popSoundObject = new Audio.Sound();
-    const pop2SoundObject = new Audio.Sound();
-    const winSoundObject = new Audio.Sound();
-    const lossSoundObject = new Audio.Sound();
-    const drawSoundObject = new Audio.Sound();
-
-    const loadSounds = async () => {
-      /* eslint-disable @typescript-eslint/no-var-requires */
-      await popSoundObject.loadAsync(require("@assets/pop-1.wav"));
-      popSoundRef.current = popSoundObject;
-      await pop2SoundObject.loadAsync(require("@assets/pop-2.wav"));
-      pop2SoundRef.current = pop2SoundObject;
-      await winSoundObject.loadAsync(require("@assets/win.wav"));
-      winSoundRef.current = winSoundObject;
-      await lossSoundObject.loadAsync(require("@assets/loss.wav"));
-      lossSoundRef.current = lossSoundObject;
-      await drawSoundObject.loadAsync(require("@assets/draw.wav"));
-      drawSoundRef.current = drawSoundObject;
-    };
-
-    loadSounds();
-    return () => {
-      // unload sounds
-      popSoundObject && popSoundObject.unloadAsync();
-      pop2SoundObject && pop2SoundObject.unloadAsync();
-      winSoundObject && winSoundObject.unloadAsync();
-      lossSoundObject && lossSoundObject.unloadAsync();
-      drawSoundObject && drawSoundObject.unloadAsync();
-    };
-  }, []);
 
   return (
     <BackgroundGradient>
